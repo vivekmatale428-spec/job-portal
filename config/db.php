f1b9e28d9e7ca85f4e4968a114c2c0996655326b
sagar <sagarshedage2005@gmail.com>
2026-02-19 16:13:16 +0530

    Added Docker setup

--- config/db.php
+++ config/db.php
@@ -7,7 +7,7 @@ require_once __DIR__ . '/constants.php';
 
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
-define('DB_PASS', '123');
+define('DB_PASS', '');
 define('DB_NAME', 'job_portal');
 
 $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
