4d8feb78e61ac5ebcd7d6ccc77372fe7aeaff021
sagar <sagarshedage2005@gmail.com>
2026-02-24 08:40:38 +0530

    Final DB connection

--- config/db.php
+++ config/db.php
@@ -1,13 +1,6 @@
 <?php
 
-$database_url = getenv("DATABASE_URL");
-
-if (!$database_url) {
-    // जर environment variable नसेल तर direct URL वापर
-    $database_url = "postgresql://USERNAME:PASSWORD@dpg-d6beq00boq4c73fjl5h0-a/job_portal_i8bj";
-}
-
-$conn = pg_connect($database_url);
+$conn = pg_connect("host=dpg-d6beq00boq4c73fjl5h0-a.oregon-postgres.render.com port=5432 dbname=job_portal_i8bj user=job_portal_she8_user password=MCnBolTmE4a4z7qMIMzNdePsDuoFmQ56");
 
 if (!$conn) {
     die("Database connection failed.");
