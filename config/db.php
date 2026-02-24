32d28904b2621e080c14a33a77dd98f6e2884301
sagar <sagarshedage2005@gmail.com>
2026-02-24 08:43:37 +0530

    Updated db connection

--- config/db.php
+++ config/db.php
@@ -1,6 +1,12 @@
 <?php
 
-$conn = pg_connect("host=dpg-d6beq00boq4c73fjl5h0-a.oregon-postgres.render.com port=5432 dbname=job_portal_i8bj user=job_portal_she8_user password=MCnBolTmE4a4z7qMIMzNdePsDuoFmQ56");
+$conn = pg_connect(
+"host=dpg-d6beq00boq4c73fjl5h0-a.oregon-postgres.render.com 
+port=5432 
+dbname=job_portal_i8bj 
+user=job_portal_she8_user 
+password=MCnBolTmE4a4z7qMIMzNdePsDuoFmQ56"
+);
 
 if (!$conn) {
     die("Database connection failed.");
